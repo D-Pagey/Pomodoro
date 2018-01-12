09ad5d9af1772234e7dc4e797e4000ac7728609a
Pagey <dan.page91@gmail.com>
2018-01-12 20:37:59 +0000

    Functional mobile layout.

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// const clockface = document.querySelector(".clockface");
+//
+// let current = clockface.innerHTML;
+//
+// function countdown() {
+//   current -= 1;
+//   clockface.innerHTML = current;
+// }
+//
+// var x = setInterval(function countdown() {
+//   current -= 1;
+//   clockface.innerHTML = current;
+//   if (current == 0) {
+//     clearInterval(x);
+//   }
+// }, 1000);
+//
+// console.log(x);
