ee7f9d4d4267c17bb2a2fb3e2c4d9e1f77f05583
Pagey <dan.page91@gmail.com>
2018-01-13 14:42:30 +0000

    Linked mainTimer, breakTimer and Tally.

--- scripts.js
+++ scripts.js
@@ -1,54 +1,80 @@
 /**
   * Selectors
  */
-const clock = document.querySelectorAll(".clockface");
+const clocks = document.querySelectorAll(".clockface");
 const btns = document.querySelectorAll(".btn");
 
 /**
   * Variables
  */
-let mainCount = clock[0].innerHTML;
+let mainCount = clocks[0].innerHTML;
+let breakCount = clocks[1].innerHTML;
+let tallyCount = 0;
 let intervalID;
-
-/**
-  * Create 2 minute clock
-*/
-let mins;
-let secs;
-
-
 
  /**
    * Methods
   */
-function countdown() {
+function mainCountdown() {
   if (mainCount > 0) {
     mainCount -= 1;
-    clock[0].innerHTML = mainCount;
+    clocks[0].innerHTML = mainCount;
   } else if (mainCount == 0) {
     clearInterval(intervalID);
-    console.log("Finished");
+    tally();
+    console.log("Finished work, now for a break.");
+    startBreak();
   }
 }
 
-function start(e) {
-  intervalID = setInterval(countdown, 1000);
+function breakCountdown() {
+  if (breakCount > 0) {
+    breakCount -= 1;
+    clocks[1].innerHTML = breakCount;
+  } else if (breakCount == 0) {
+    clearInterval(intervalID);
+    console.log("Finished break, now for more work.");
+    reset();
+    start();
+  }
 }
 
-function reset(e) {
-  clock[0].innerHTML = "25:00";
+function start() {
+  intervalID = setInterval(function() {
+    mainCountdown();
+  }, 1000);
+}
+
+function startBreak() {
+  intervalID = setInterval(function() {
+    breakCountdown();
+  }, 1000);
+}
+
+function reset() {
+  clocks[0].innerHTML = 6;
+  clocks[1].innerHTML = 5
+  mainCount = clocks[0].innerHTML;
+  breakCount = clocks[1].innerHTML;
+}
+
+function tally() {
+  tallyCount++;
+  clocks[2].innerHTML = tallyCount;
 }
 
 /**
   * Events
  */
-btns[5].addEventListener("click", start);
-btns[2].addEventListener("click", reset);
+btns[6].addEventListener("click", start);
+btns[4].addEventListener("click", reset);
 
 /**
-  * To Do:
-  * 1) Break timer to start after main has Finished
-  * 2)
+  To Do:
+  1) Build 2 minute clock
+  2) Implement clock into timers
+  3) Inc/Dec functionality
+  4) Pause button
 */
 
 /**
@@ -57,4 +83,5 @@ btns[2].addEventListener("click", reset);
 3) Countdown to stored date + timer
 4) Once hit, +1 to tally, start break counter
 5) Once tally = 4 then break timer = 20 mins
+* Break timer to start after main has Finished
 */
