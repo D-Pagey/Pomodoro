05e759c1f01f58732d40ec8b23e167adc35a3201
D-Pagey <dan.page91@gmail.com>
2018-02-17 21:02:31 +0000

    Starting to restyle. Scripted a deadline calculator.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 /**
   * Selectors
  */
-const clocks = document.querySelectorAll(".clockface");
-const btns = document.querySelectorAll(".btn");
+const modalBtn = document.getElementsByClassName('modal-btn');
+const modal = document.getElementsByClassName('modal-about');
+const cancel = document.getElementsByClassName('modal-cancel');
+const clocks = document.querySelectorAll('.clockface');
+const btns = document.querySelectorAll('.btn');
 
 /**
   * Variables
  */
+let defaultTime = 25;
 let mainCount = clocks[0].innerHTML;
 let breakCount = clocks[1].innerHTML;
 let tallyCount = 0;
@@ -22,7 +26,7 @@ function mainCountdown() {
   } else if (mainCount == 0) {
     clearInterval(intervalID);
     tally();
-    console.log("Finished work, now for a break.");
+    console.log('Finished work, now for a break.');
     startBreak();
   }
 }
@@ -33,7 +37,7 @@ function breakCountdown() {
     clocks[1].innerHTML = breakCount;
   } else if (breakCount == 0) {
     clearInterval(intervalID);
-    console.log("Finished break, now for more work.");
+    console.log('Finished break, now for more work.');
     reset();
     start();
   }
@@ -69,32 +73,39 @@ function tally() {
 btns[6].addEventListener("click", start);
 btns[4].addEventListener("click", reset);
 
-/**
-  To Do:
-  1) Build 2 minute clock
-  2) Implement clock into timers
-  3) Inc/Dec functionality
-  4) Pause button
-*/
-
-/**
-1) Store current Date + Time
-2) add maintimer.innerHTMl to the Date + Time
-3) Countdown to stored date + timer
-4) Once hit, +1 to tally, start break counter
-5) Once tally = 4 then break timer = 20 mins
-* Break timer to start after main has Finished
-*/
-
-// Modal Functionality
-const modalBtn = document.getElementsByClassName('modal-btn');
-const modal = document.getElementsByClassName('modal-about');
-const button = document.getElementsByClassName('modal-cancel');
-
 modalBtn[0].addEventListener('click', function() {
   modal[0].showModal();
 });
 
-button[0].addEventListener('click', function() {
+cancel[0].addEventListener('click', function() {
   modal[0].close();
 })
+
+/**
+  * Countdown tutorial
+ */
+const currentTime = new Date();
+const deadline = new Date(currentTime.getTime() + (20 * 60 * 1000));
+
+function timeRemaining(deadline) {
+  const t = Date.parse(deadline) - Date.parse(new Date());
+  const seconds = Math.floor( (t/1000) % 60 );
+  const minutes = Math.floor( (t/1000/60) % 60 );
+
+  return {
+    'total': t,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+/* COUNTDOWN PLAN
+1) Get current time
+2) Add Pomodoro amount to current time, store in variable targetTime
+3) Countdown until current time == targetTime
+4) Basic modal says done
+5) Launch break timer
+6) Increment tally
+7) Modal to say get back to work
+8) If tally == 4, then breaktimer = 20 mins
+*/
